536c67457dcb5acde6c67515fdb61f8614e01fb2
cai zhibiao <cai_zhibiao@163.com>
2018-10-27 21:46:31 +0800

    add maven project

---
+++ src/main/java/com/xiaoyutech/ms/Person.java
@@ -0,0 +1,11 @@
+package com.xiaoyutech.ms;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Person {
+    @Getter
+    private String name;
+
+}
---
+++ src/main/java/com/xiaoyutech/ms/controller/first.java
@@ -0,0 +1,9 @@
+package com.xiaoyutech.ms.controller;
+
+import com.xiaoyutech.ms.Person;
+
+public class first {
+    public  void main(String args[]){
+//        new Person().getName();
+    }
+}
